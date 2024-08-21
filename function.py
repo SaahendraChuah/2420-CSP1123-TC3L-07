8476f4e5d539eda34f26916ff41b09f4210704c2
Hemaraj <rideralex118@gmail.com>
2024-08-21 12:19:55 +0800

    trying database

--- function.py
+++ function.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
 app=Flask(__name__) #creates flask application object
+app.config.from_mapping(SECRET_KEY="sqlite:/database.db")
+database1=SQLAlchemy(app)
+
+class Login_Register(database1.Model):
+    
+    Username=database1.Column(database1.Text, nullable=False) # does not accept any null values
+    Student_Email=database1.Column(database1.Text, nullable=False)
+    Password=database1.Column(database1.Password, nullable=False)
+    Phone_Number=database1.Column(database1.Text, nullable=False)
+
+
 
 
 if __name__== "main":
